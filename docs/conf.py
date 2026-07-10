1497031e7beae68cc5bd4682c009357f9cb1d300
lazytatzv <yano.tatsuki439@mail.kyutech.jp>
2026-07-10 09:01:10 +0900

    docs: switch from Doxygen to Sphinx with ReadTheDocs theme

---
+++ docs/conf.py
@@ -0,0 +1,19 @@
+project = 'libbno055-linux'
+copyright = '2026, lazytatzv'
+author = 'lazytatzv'
+release = '1.0.0'
+
+extensions = [
+    'myst_parser',
+    'sphinx_rtd_theme',
+]
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+html_theme = 'sphinx_rtd_theme'
