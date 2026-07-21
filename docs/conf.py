911bda30857226eb17ef9b643ea15910ec89d143
lazytatzv <yano.tatsuki439@mail.kyutech.jp>
2026-07-22 05:41:42 +0900

    chore: bump version to 1.5.5 for PyPI release and add sdist MANIFEST

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 project = 'libbno055-linux'
 copyright = '2026, lazytatzv'
 author = 'lazytatzv'
-release = '1.5.4'
+release = '1.5.5'
 
 extensions = [
     'myst_parser',
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ ext_modules = [
 
 setup(
     name="libbno055-linux",
-    version="1.5.4",
+    version="1.5.5",
     author="lazytatzv",
     author_email="lazytatzv@users.noreply.github.com",
     url="https://github.com/lazytatzv/libbno055-linux",
