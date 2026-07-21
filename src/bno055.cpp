2f39ee4083987bbfc06ed20e9e48c84e9e96ffa1
lazytatzv <yano.tatsuki439@mail.kyutech.jp>
2026-07-22 07:42:53 +0900

    feat(ros2): expose UART ASYNC_LOW_LATENCY and SCHED_FIFO thread priority to YAML

--- src/bno055.cpp
+++ src/bno055.cpp
@@ -4,6 +4,8 @@
 #include <poll.h>
 #include <sys/ioctl.h>
 #include <termios.h>
+#include <unistd.h>
+#include <linux/serial.h>
 
 #include "libbno055-linux/transport.hpp"
 
@@ -265,6 +267,18 @@ public:
                 i2c_fd = -1;
                 return false;
             }
+
+            if (uart_config_.low_latency) {
+                struct serial_struct serial;
+                if (ioctl(i2c_fd, TIOCGSERIAL, &serial) == 0) {
+                    serial.flags |= ASYNC_LOW_LATENCY;
+                    ioctl(i2c_fd, TIOCSSERIAL, &serial);
+                    log(LogLevel::Info, "UART Low Latency mode enabled via TIOCSSERIAL.");
+                } else {
+                    log(LogLevel::Warning, "Failed to enable UART Low Latency mode (TIOCGSERIAL failed).");
+                }
+            }
+
             return true;
         }
 
