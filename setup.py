25cb86025711a0bb952906bcbdad196b4f2e1d0e
lazytatzv <yano.tatsuki439@mail.kyutech.jp>
2026-07-29 14:54:36 +0900

    chore: bump version to 1.8.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ ext_modules = [
 
 setup(
     name="libbno055-linux",
-    version="1.8.0",
+    version="1.8.1",
     author="lazytatzv",
     author_email="lazytatzv@users.noreply.github.com",
     url="https://github.com/lazytatzv/libbno055-linux",
