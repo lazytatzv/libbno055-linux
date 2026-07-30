a4ce4e39e65d8eaf65ceaa76fc0ff86c3c082b4b
lazytatzv <yano.tatsuki439@mail.kyutech.jp>
2026-07-30 16:31:08 +0900

    refactor(rust): eliminate duplicated cpp source files by linking directly to core C++ library

--- rust/build.rs
+++ rust/build.rs
@@ -1,14 +1,23 @@
+use std::path::PathBuf;
+
 fn main() {
+    let manifest_dir = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
+    let root_dir = manifest_dir.parent().unwrap();
+
+    let include_dir = root_dir.join("include");
+    let src_bno055 = root_dir.join("src").join("core").join("bno055.cpp");
+    let src_bno055_c = root_dir.join("src").join("core").join("bno055_c.cpp");
+
     cc::Build::new()
         .cpp(true)
         .std("c++17")
-        .include("cpp/include")
-        .file("cpp/src/bno055.cpp")
-        .file("cpp/src/bno055_c.cpp")
+        .include(&include_dir)
+        .file(&src_bno055)
+        .file(&src_bno055_c)
         .compile("bno055-linux");
 
-    println!("cargo:rerun-if-changed=cpp/include/libbno055-linux/bno055_c.h");
-    println!("cargo:rerun-if-changed=cpp/include/libbno055-linux/bno055.hpp");
-    println!("cargo:rerun-if-changed=cpp/src/bno055.cpp");
-    println!("cargo:rerun-if-changed=cpp/src/bno055_c.cpp");
+    println!("cargo:rerun-if-changed={}", root_dir.join("include").join("libbno055-linux").join("bno055_c.h").display());
+    println!("cargo:rerun-if-changed={}", root_dir.join("include").join("libbno055-linux").join("bno055.hpp").display());
+    println!("cargo:rerun-if-changed={}", src_bno055.display());
+    println!("cargo:rerun-if-changed={}", src_bno055_c.display());
 }
